81b9aed510810c3a8ea3548fd8a367010d13d19f
Xueying Wang <daiszws19@gmail.com>
2023-05-31 21:36:43 +0100

    Add post request

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const videoRoute = require("./routes/videos");
 const cors = require("cors");
 
 app.use(cors());
-
+app.use(express.json());
+app.use(express.static("public"));
 app.use("/videos", videoRoute);
 
 app.listen(PORT, () => {
--- routes/videos.js
+++ routes/videos.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const videoData = require("../data/videos.json");
 const crypto = require("crypto");
 
-
 // Get all videos
 
 router.get("/", (req, res) => {
@@ -14,20 +13,56 @@ router.get("/", (req, res) => {
   res.json(videos);
 });
 
-
 // Get single video
-router.get("/:id", (req, res)=> {
-    const specificVideo = videoData.find((video)=> {
-      return  video.id === req.params.id
-    });
+router.get("/:id", (req, res) => {
+  const specificVideo = videoData.find((video) => {
+    return video.id === req.params.id;
+  });
 
-    if(!specificVideo) {
-        res.status(404).send("Video is not found");
-    }
+  if (!specificVideo) {
+    res.status(404).send("Video is not found");
+  }
 
-    res.json(specificVideo);
-})
+  res.json(specificVideo);
+});
 
+// Post a new video
+router.post("/", (req, res) => {
+  const { title, description } = req.body;
 
+  if (!title || !description) {
+    res.status(400).send("Please fill in all fields");
+  }
+
+  const newVideo = {
+    id: crypto.randomUUID(),
+    title: title,
+    description: description,
+    channel: "Daisy Wang",
+    image: "../public/Upload-video-preview.jpg",
+    views: "2,001,023",
+    likes: "210,985",
+    timestamp: new Date(),
+    comments: [
+      {
+        id: "35bba08b-1b51-4153-ba7e-6da76b5ec1b9",
+        name: "Micheal Lyons",
+        comment: "That's so coo!",
+        likes: 3,
+        timestamp: 1628522461000,
+      },
+      {
+        id: "091de676-61af-4ee6-90de-3a7a53af7521",
+        name: "Gary Wong",
+        comment: "How can someone be so good!!!",
+        likes: 0,
+        timestamp: 1626011132000,
+      },
+    ],
+  };
+  videoData.push(newVideo);
+
+  res.status(201).json(newVideo);
+});
 
 module.exports = router;
